177beb4d0ffee06965554953653678a069d5b2a6
Renjith-38 <renjithphilip.mail@gmail.com>
2021-05-14 23:14:19 +0530

    Added delete option

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,28 +1,82 @@
 const express = require("express");
 const adminRouter = express.Router();
 const Bookdata = require("../model/Bookdata");
+const AuthorData = require("../model/Authordata");
+
+//file storage
+const multer = require('multer');
+
+
+//specify location to save images(multer)
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null, 'public/images');
+    },
+    filename: function(req,file,cb){
+        cb(null, Date.now() + file.originalname);
+    }
+});
+
+//storage varaible
+const store = multer({storage:storage});
 
 function router(nav){
-    adminRouter.get('/',function(req,res){
+    adminRouter.get('/addBookPage',function(req,res){
         res.render('addBook',{
         nav,
-        title:'Library'
+        title:'Add Book'
         });
     });
 
-    adminRouter.post('/add',function(req,res){
+    //route for hadnling add book request
+    adminRouter.post('/addbook',store.single('image'),function(req,res){
         var item = {
 
             title: req.body.title,
             author: req.body.author,
             genre: req.body.genre,
-            image: req.body.image
+            description: req.body.description,
+            image: req.file.filename
         }
         var book = Bookdata(item);
         book.save(); //saving to database
         res.redirect('/books');
         
         
+    });
+
+    adminRouter.get('/addAuthorPage',function(req,res){
+        res.render('addAuthor',{
+        nav,
+        title:'Add Author'
+        });
+    });
+
+    //route for hadnling add author request
+    adminRouter.post('/addauthor',store.single('image'),function(req,res){
+        var item = {
+
+            name:req.body.name,
+            description:req.body.description,
+            image:req.file.filename
+        }
+        var author = AuthorData(item);
+        author.save(); //saving to database
+        res.redirect('/authors');
+        
+        
+    });
+
+    //route for handling book delete request
+    adminRouter.post('/deleteBook/:id',async (req,res)=>{
+        await Bookdata.findByIdAndDelete({_id:req.params.id});
+        res.redirect('/books');
+    });
+
+    //route for handling author delete request
+    adminRouter.post('/deleteAuthor/:id',async (req,res)=>{
+        await AuthorData.findByIdAndDelete({_id:req.params.id});
+        res.redirect('/authors');
     });
 
     return adminRouter;
